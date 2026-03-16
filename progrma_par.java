7e66aeb8554651a090c31af92766458b85c8dfa2
daniel97 <dani.nicotina@gmail.com>
2026-03-16 10:39:02 -0400

    numeros pares

---
+++ progrma_par.java
@@ -0,0 +1,11 @@
+public class progrma_par {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) { // Comprueba si el residuo es 0
+                System.out.println(i);
+            }
+        }
+    }
+
+}
