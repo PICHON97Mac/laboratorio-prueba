0e597859c620466468bb5516b45e43a6a824b589
daniel97 <dani.nicotina@gmail.com>
2026-03-16 11:18:17 -0400

    java sin comentarios

--- progrma_par.java
+++ progrma_par.java
@@ -2,7 +2,7 @@ public class progrma_par {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 2 == 0) { // Comprueba si el residuo es 0
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
